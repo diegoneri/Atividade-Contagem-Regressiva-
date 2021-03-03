76fc824b7d4ad74ca538d905f4169a409351363f
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-03 20:31:51 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Atividade_Contagem_Regressiva_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.WriteLine("Apontar...");
+            Console.WriteLine("FOGO!");
+
+        }
+    }
+}
