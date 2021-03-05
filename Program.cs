0c1ba22d0131f00baa881419ecd02531e7338877
Diego Neri <diego@diegoneri.com>
2021-03-05 07:17:19 -0300

    Adiciona pausa entre as frases
    O comando Console.ReadKey(); faz com que o programa aguarde que o usuário pressione uma tecla.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace Atividade_Contagem_Regressiva_
         static void Main(string[] args)
         {
             Console.WriteLine("Preparar...");
+            Console.ReadKey();
             Console.WriteLine("Apontar...");
+            Console.ReadKey();
             Console.WriteLine("FOGO!");
 
         }
